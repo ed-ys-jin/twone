0c6eb43840e375c3eecb306a6e6e98a43cb86176
ed-ys-jin <ed.ys.jin@gmail.com>
2023-01-12 18:15:44 +0900

    [twone_jin] 이메일 정규식 수정 230112/18:15

--- src/main/java/com/shinjin/twone/dto/MemDTO.java
+++ src/main/java/com/shinjin/twone/dto/MemDTO.java
@@ -12,7 +12,7 @@ public class MemDTO {
     private int memSeq;
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Pattern(regexp = "/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i"
+    @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"
             , message = "이메일 형식에 맞지 않습니다.")
     private String memEmail;
 
