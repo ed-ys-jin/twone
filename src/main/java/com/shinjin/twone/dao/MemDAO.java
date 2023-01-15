05ee203f3546cfbf53c8622c64703b0d4538cf1e
ed-ys-jin <ed.ys.jin@gmail.com>
2023-01-16 02:58:43 +0900

    [twone_jin] 1. redirect 방식 적용 2. 변수명 규칙 변경 230116/02:58

--- src/main/java/com/shinjin/twone/dao/MemDAO.java
+++ src/main/java/com/shinjin/twone/dao/MemDAO.java
@@ -6,11 +6,11 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemDAO {
     public int checkDupl(String email); // 중복 이메일 확인
-    public int signup(MemDTO memDto); // 회원 등록
-    public MemDTO login(MemDTO memDto); // 로그인
-    public int withdraw(MemDTO memDto); // 회원탈퇴
+    public int signup(MemDTO memDTO); // 회원 등록
+    public MemDTO login(MemDTO memDTO); // 로그인
+    public int withdraw(MemDTO memDTO); // 회원탈퇴
     public MemDTO getDto(int memSeq); // memDTO 불러오기
-    public int updateMemInfo(MemDTO memDto); // 회원정보 수정
+    public int updateMemInfo(MemDTO memDTO); // 회원정보 수정
     public String getPw(int memSeq); // 비밀번호 불러오기
-    public int changePw(MemDTO memDto); // 비밀번호 변경
+    public int changePw(MemDTO memDTO); // 비밀번호 변경
 }
