f6a44b08748aea58152b04c10c478c09a1afa381
ed-ys-jin <ed.ys.jin@gmail.com>
2023-01-16 01:29:55 +0900

    [twone_jin] 1. function : 로그인 인터셉터 230116/01:29

--- src/main/java/com/shinjin/twone/config/InterceptorConfig.java
+++ src/main/java/com/shinjin/twone/config/InterceptorConfig.java
@@ -1,4 +1,20 @@
 package com.shinjin.twone.config;
 
-public class InterceptorConfig {
+import com.shinjin.twone.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        /* 로그인 인터셉터 */
+        registry.addInterceptor(new LoginInterceptor()) // 인터셉터 등록
+                .addPathPatterns("/**") // 인터셉터를 적용할 url 패턴
+                .excludePathPatterns("/login", "/signup", "/resources/**"); // 인터셉터에서 제외할 패턴 지정
+    }
+
 }
