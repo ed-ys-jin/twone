d00051106dc7d2dd408484cd4d2842977acc982d
ed-ys-jin <ed.ys.jin@gmail.com>
2023-01-15 16:05:11 +0900

    [twone_jin] 1. 오류 수정 : Team* 파일 내 TestDTO 코드 삭제 230115/16:05

--- src/main/java/com/shinjin/twone/dao/TeamDAO.java
+++ src/main/java/com/shinjin/twone/dao/TeamDAO.java
@@ -1,14 +1,14 @@
 package com.shinjin.twone.dao;
 
 import com.shinjin.twone.dto.MemDTO;
-import com.shinjin.twone.dto.TestDTO;
+import com.shinjin.twone.dto.TeamDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface TeamDAO {
-  public TestDTO selectList();
+  public TeamDTO selectList();
 
   public List<MemDTO> selectTeamList();
 
