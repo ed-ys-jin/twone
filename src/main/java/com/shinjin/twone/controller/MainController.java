eb503461b1e91bd747fe4ed8fb52ed6e9b58f873
ed-ys-jin <ed.ys.jin@gmail.com>
2023-02-05 22:14:13 +0900

    [twone_jin] 1. 배포 테스트 작업

--- src/main/java/com/shinjin/twone/controller/MainController.java
+++ src/main/java/com/shinjin/twone/controller/MainController.java
@@ -3,11 +3,17 @@ package com.shinjin.twone.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller("mainController")
+@Controller
 public class MainController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
-    public String home() throws Exception{
-        return "index";
+    public ModelAndView home() throws Exception{
+        ModelAndView mav = new ModelAndView();
+
+        mav.setViewName("index");
+
+        return mav;
+//        return "index";
     }
 }
