999196fa37e74039f72ef60594ba4fba8446979c
ed-ys-jin <ed.ys.jin@gmail.com>
2023-02-05 22:34:18 +0900

    [twone_jin] 1. 배포 테스트 작업

--- src/main/java/com/shinjin/twone/controller/MainController.java
+++ src/main/java/com/shinjin/twone/controller/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller("mainController")
+@Controller
 public class MainController {
     @RequestMapping(value= "/", method = RequestMethod.GET)
     public String home() throws Exception{
